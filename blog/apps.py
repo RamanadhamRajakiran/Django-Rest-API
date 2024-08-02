b71699406a09c2932a2866122705a01c414d5d5c
RamanadhamRajakiran <ramanadhamrajkiran@gmail.com>
2024-08-02 13:31:06 +0530

    Django-Rest-API-v1
    Added v1

--- blog/apps.py
+++ blog/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-# Configuration 
+# Configuration v1
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
